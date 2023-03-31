c9c102235eb9846204b5f99c8e135fc63f568998
jaime castaño <jimmysilver11@outlook.es>
2023-03-30 23:14:31 -0500

    Pagination and filter full

--- Sales.API/Helpers/QueryableExtensions.cs
+++ Sales.API/Helpers/QueryableExtensions.cs
@@ -5,8 +5,7 @@ namespace Sales.API.Helpers
 {
     public static class QueryableExtensions
     {
-        public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable,
-            PaginationDTO pagination)
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO pagination)
         {
             return queryable
                 .Skip((pagination.Page - 1) * pagination.RecordsNumber)
